b27c5ec866dc8ece416a11db0b83f8e0c1057da1
jolson615 <jolson615@gmail.com>
2019-07-19 10:40:22 -0400

    adds files

--- profile_class.py
+++ profile_class.py
@@ -1,1 +1,2 @@
+# View the readme on Github to get more information on how to start this definition. 
 class Profile:
--- test.py
+++ test.py
@@ -1,1 +1,2 @@
+# View the readme on GitHub for more information about how to start this challenge. 
 import profile_class
